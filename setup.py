9fe35a136240676fcfc20f83cf33db32962a7243
Sebastien Hubert <sebastien.hubert.apple@gmail.com>
2022-11-28 14:03:47 +0100

    complete setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='deep_draw',
+      version="0.0.1",
+      description="Deep Draw Project",
+      license="MIT",
+      author="Sythak",
+      author_email="contact@lewagon.org",
+      url="https://github.com/Sythak/deepdraw",
+      install_requires=requirements,
+      packages=find_packages(),
+      )
